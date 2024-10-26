ecfad777440f5fa1c4220a6c527b80f49aaeccd2
Onur Boran Dalkilinc <odalkili@k1m37s01.42istanbul.com.tr>
2024-10-26 22:20:52 +0300

    second commit

---
+++ ft_memcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odalkili <odalkili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 21:01:49 by odalkili          #+#    #+#             */
+/*   Updated: 2024/10/26 21:38:42 by odalkili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    unsigned char   *tmp_dst;
+    unsigned char   *tmp_src;
+    
+    if(dst == (void *)0 && src == (void *)0)
+        return (dst);
+    tmp_dst = (unsigned char *) dst;
+    tmp_src = (unsigned char *) src;
+
+    while (n > 0)
+    {
+        *(tmp_dst++) = *(tmp_src++);
+        n--;
+    }
+       return (dst);
+}
---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odalkili <odalkili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 20:09:15 by odalkili          #+#    #+#             */
+/*   Updated: 2024/10/26 21:20:28 by odalkili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memset(void *b, int c, size_t len)
+{
+	unsigned char    *tmp_ptr;
+	
+	tmp_ptr = (unsigned char *) b;
+	while (len > 0)
+	{
+		*(tmp_ptr++) = (unsigned char) c;
+		len--;
+	}
+	return (b);
+}
